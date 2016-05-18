3a20817ae7fc65578e63e9b8567b2c99593cc60f
SlobodanV <vujo00@gmail.com>
2016-05-18 08:59:06 +0200

    after throwing East String fields from FrontWindow

---
+++ reportGenerator/frontWindowTest.java
@@ -0,0 +1,12 @@
+package reportGenerator;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class frontWindowTest {
+
+	@Test
+	public void test() {
+
+	}
+}
---
+++ reportGenerator/masterTest.java
@@ -0,0 +1,12 @@
+package reportGenerator;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ inputFilesTest.class, frontWindowTest.class })
+
+public class masterTest {
+
+}
